5bb3558a15d2256a5700dedd7d015a4335af78f8
Mac <mac@Macs-MacBook-Pro.local>
2024-07-01 13:53:02 +0545

    initial

---
+++ admin/partials/footer.php
@@ -0,0 +1,12 @@
+<!--footer section starts here-->
+<div class="footer">
+             <div class="wrapper">
+                <p class="text-center">BCA 4th Sem Project E-Tech by Rijan & Abisha</p>
+            </div>
+            
+        </div>
+
+        
+         <!-- footer section ends here-->
+    </body>
+</html>
---
+++ partials-front/menu.php
@@ -0,0 +1,59 @@
+<?php include('config/constants.php');?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!--imp to make website responsive-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Tech</title>
+
+    <!-- link css-->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/admin.css">
+
+</head>
+<body>
+    <!--Navbar section start-->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <img src="images/logo .png" alt="E-Tech logo" class="img-responsive">
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="<?php echo SITEURL; ?>">Home</a>
+                    </li>
+                    <!-- Show menu according to login status -->
+                    <?php if (isset($_SESSION["user_logged_in"]) && $_SESSION["user_logged_in"] === true) : ?>
+                        <li>
+                            <a href="<?php echo SITEURL; ?>categories.php">Categories</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo SITEURL; ?>items.php">Items</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo SITEURL; ?>logout.php">Logout</a>
+                    </li>
+                    <?php else: ?>
+                        <li>
+                            <a href="<?php echo SITEURL; ?>register.php">Sign Up</a>
+                        </li>
+                        <li>
+                            <a href="<?php echo SITEURL; ?>login.php">Login</a>
+                        </li>
+                    <?php endif; ?>
+                </ul>
+            </div>
+            
+            <div class="clearfix">
+
+            </div>
+           
+        </div>
+        
+    </section>
+
+    <!--Navbar section end-->
